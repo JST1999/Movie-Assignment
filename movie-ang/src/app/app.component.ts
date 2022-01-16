efaba4171ae631c23a3170e3e6a4d617f420f56e
Jason Tungay <jtungay@gmail.com>
2022-01-16 21:08:15 +0000

    all the movie info except poster is now displayed

--- movie-ang/src/app/app.component.ts
+++ movie-ang/src/app/app.component.ts
@@ -7,15 +7,44 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private httpClient: HttpClient){}
+  constructor(
+    private httpClient: HttpClient
+  ){}
 
+  apikey = "4e157d55"
   title = 'movie-ang';
-  movies=[{title: 'Nothing here yet', poster: '', plot: ''}];
+  movies=[{id: "Nothing here yet", title: '', poster: '', plot: ''}];
   items:movies | undefined
-  getMovies(){
-    this.httpClient.get<movies>("http://localhost:8100/movies/1").toPromise().then(data => {
+  name = "No one fetched"
+
+  getMovieDetails(id: string){
+    this.httpClient.get<movieDetails>(`http://www.omdbapi.com/?i=${id}&apikey=${this.apikey}`).toPromise().then(data => {
+      this.movies.push({id: id, title: data?.Title!, poster: '', plot: data?.Plot!})
+    })
+  }
+
+  getMovies(id: HTMLTextAreaElement){
+    this.httpClient.get<movies>(`http://localhost:8100/movies/${id.value}`).toPromise().then(data => {
       this.items = data
       console.log(this.items)
+
+      if (this.items?.id != null){
+        this.name = `${this.items?.id} | ${this.items?.firstName} ${this.items?.lastName}`
+        var favMovies = this.items?.favourite_movies.split(',')
+        if (favMovies.length > 0){
+          this.movies = []
+          favMovies?.forEach(element => {
+            console.log(element)
+            this.getMovieDetails(element)
+          });
+        } else{
+          this.movies=[{id: "Nothing here yet", title: '', poster: '', plot: ''}];
+        }
+      } else{
+        this.name = "No one fetched"
+        this.movies=[{id: "Nothing here yet", title: '', poster: '', plot: ''}];
+      }
+
     })
   }
 
@@ -25,4 +54,8 @@ interface movies {
   firstName: string;
   lastName: string;
   favourite_movies: string;
+}
+interface movieDetails {
+  Title: string;
+  Plot: string;
 }
